e05449d7c382d77826ea0c3c27f813b83c7c858b
nelcandido <nelcandido@gmail.com>
2025-01-07 16:05:33 -0300

    Primeiro commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "nome" {
+  type        = string
+  description = "Nome da instância"
+}
+
+variable "environment" {
+  type        = string
+  description = "Ambiente da instância"
+  default     = "dev"
+}
+
+variable "ebs_block_device" {
+  description = "lista de volumes ebs para criado e montado na instancia"
+  type        = list(any)
+}
